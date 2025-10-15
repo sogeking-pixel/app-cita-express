8d37b6f5f0c3ee63ddfcb559bd22a0d6ee57c4fb
yerson <52290162+sogeking-pixel@users.noreply.github.com>
2025-10-15 00:30:42 -0500

    feat: Define HomeUiState for home screen
    This commit introduces a sealed interface `HomeUiState` to represent the different states of the home screen UI.
    
    This interface will manage the UI state for displaying a list of specialties, handling loading, and error scenarios.
    
    Changes include:
    - Creating `HomeUiState.kt`.
    - Defining a sealed interface `HomeUiState` with three possible states:
        - `Success`: Holds a list of `Specialty` objects when data is fetched successfully.
        - `Loading`: Represents the loading state while data is being fetched.
        - `Error`: Represents an error state.

--- app/src/main/java/com/example/appcitaexpress/ui/screen/Home/HomeUiState.kt
+++ app/src/main/java/com/example/appcitaexpress/ui/screen/Home/HomeUiState.kt
@@ -1,2 +1,9 @@
-package com.example.appcitaexpress.ui.screen.Home
+package com.example.appcitaexpress.ui.screen.home
 
+import com.example.appcitaexpress.data.model.Specialty
+
+sealed interface HomeUiState {
+    data class Success(val specialties: List<Specialty>) : HomeUiState
+    object  Loading : HomeUiState
+    object  Error : HomeUiState
+}
